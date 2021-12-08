02b0287f48f1768e3fabe60927b0ee8aad4f1cdc
Elroy's MacBook Pro <elroy@Elroy-MacBook-Pro.local>
2021-12-08 21:32:27 +0000

    dashboard

---
+++ Dashboard/maps/mapdata.js
@@ -0,0 +1,196 @@
+var simplemaps_ukmap_mapdata={
+  main_settings: {
+    //General settings
+		width: "responsive", //or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    popups: "detect",
+    
+		//State defaults
+		state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "https://simplemaps.com",
+    border_size: 1.5,
+    border_color: "#ffffff",
+    all_states_inactive: "no",
+    all_states_zoomable: "no",
+    
+		//Location defaults
+		location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+		//Label defaults
+		label_color: "#ffffff",
+    label_hover_color: "#ffffff",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+   
+		//Zoom settings
+		manual_zoom: "yes",
+    back_image: "no",
+    arrow_box: "no",
+    navigation_size: "40",
+    navigation_color: "#f7f7f7",
+    navigation_border_color: "#636363",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+		//Popup settings
+		popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+		//Advanced settings
+		div: "map",
+    auto_load: "yes",
+    rotate: "0",
+    url_new_tab: "yes",
+    images_directory: "default",
+    import_labels: "no",
+    fade_time: 0.1,
+    link_text: "View Website"
+  },
+  state_specific: {
+    EE: {
+      name: "East",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    EM: {
+      name: "East Midlands",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    ES: {
+      name: "Eastern Scotland",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    EW: {
+      name: "East Wales",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    GL: {
+      name: "Greater London",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    HI: {
+      name: "Highlands and Islands",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    NE: {
+      name: "North East",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    NI: {
+      name: "Northern Ireland",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    NS: {
+      name: "North Eastern Scotland",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    NW: {
+      name: "North West",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    SE: {
+      name: "South East",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    SS: {
+      name: "South Western Scotland",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    SW: {
+      name: "South West",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    WM: {
+      name: "West Midlands",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    WV: {
+      name: "West Wales and the Valleys",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    YH: {
+      name: "Yorkshire and the Humber",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    }
+  },
+  locations: {
+    "0": {
+      lat: 51.506,
+      lng: -0.127,
+      name: "London"
+    }
+  },
+  labels: {}
+};
---
+++ _posts/2021-12-08-DashBoard.md
@@ -0,0 +1,25 @@
+---
+title: "Children's Day"
+image: 
+  path: /images/picture/Dashboard.png
+  thumbnail: /images/picture/Dashboard.png
+categories:
+  - Visualization
+tags:
+  - Visualization
+last_modified_at: 2021-12-08T16:19:20-05:00
+---
+
+The Dashboard Design of group assignment:
+
+
+<figure class="align-center">
+  <a href="#"><img src="{{ '/images/picture/Dashboard.png' | absolute_url }}" alt=""></a>
+  <figcaption>Dashboard Design </figcaption>
+</figure> 
+
+
+<https://elroy-t.github.io/Dashboard/DIAMOND.html>
+
+
+
